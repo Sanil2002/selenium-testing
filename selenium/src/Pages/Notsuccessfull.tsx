3490b9ffb359c339cd3e9d6705c6c25488eb4cfe
Sanil2002 <sanilm4637@gmail.com>
2024-09-10 11:08:11 +0530

    selenuim test build

--- selenium/src/Pages/Notsuccessfull.tsx
+++ selenium/src/Pages/Notsuccessfull.tsx
@@ -1,6 +1,6 @@
 const Notsuccessfull = () => {
-    return(<>
-    <h1>Not Successfull</h1></>)
+    return (<div className="justify-center items-center min-h-screen flex">
+        <h1 className="mb-3 pb-2 text-3xl font-semibold md:text-6xl text-red-600"><span className="font-light text-black">Your landing is </span>UnSuccessfull!</h1></div>)
 }
 
 export default Notsuccessfull
--- selenium/src/Pages/Successfull.tsx
+++ selenium/src/Pages/Successfull.tsx
@@ -1,6 +1,6 @@
 const Successfull = () => {
-    return(<>
-    <h1>Successfull</h1></>)
+    return (<div className="justify-center items-center min-h-screen flex">
+        <h1 className="mb-3 pb-2 text-3xl font-semibold md:text-6xl text-red-700"><span className="font-light text-black">You have landed </span>Successfully!</h1></div>)
 }
 
 export default Successfull
